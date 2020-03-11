f1cba79f8916a4e437a22e7e1a9656fd829501da
Reshaeala <58646198+Reshaeala@users.noreply.github.com>
2020-03-11 15:34:17 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+
+
+const dbupdateobject = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+};
+
+
+// Connect to Mongo
+mongoose.connect(process.env.DATABASE_URL, dbupdateobject);
+// Connection Error/Success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected!')
+);
+db.on('disconnected', () => console.log('mongo disconnected'));
+db.on('open', () => {
+    console.log('Connection made!');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+})
